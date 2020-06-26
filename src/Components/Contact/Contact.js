dbe9963291a9b3ee9db0e661356fbcb260236c46
Norrismi <Norrismi0@gmail.com>
2020-06-25 22:52:27 -0400

    working on contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import Nav from '../Nav/Nav'
-import './StylesContact.css'
-import api from '../ThirdParty/API'
+import Nav from "../Nav/Nav";
+import "./StylesContact.css";
+import api from "../ThirdParty/API";
+import Footer from "../Nav/Footer";
+import shSax from "../../Assets/shSax.jpg";
 
-
-//let emailRegex = RegExp(/[^@]+@[^.]+\..+/);
+let emailRegex = RegExp(/[^@]+@[^.]+\..+/);
 
 //const api = process.env.REACT_APP_ENDPOINT;
-
 
 class Contact extends Component {
   state = {
@@ -15,6 +15,7 @@ class Contact extends Component {
     phone: "",
     email: "",
     message: "",
+    success:'',
 
     formErrors: {
       email: "",
@@ -34,18 +35,19 @@ class Contact extends Component {
 
     this.state.phone && this.state.message
       ? api.post("/submission.json", data).then((res) => {
-          console.log(res)
-          this.props.history.push("/ContactComplete");
-        }
-        )
-        // .then(error => {console.log(error)})
-      : data.formErrors.form.push("Complete the form");
+          console.log(res);
+          data.success.push('Your Information was Submitted Thanks!')
+          // this.props.history.push("/ContactComplete");
+        })
+      : // .then(error => {console.log(error)})
+        data.formErrors.form.push("Complete the form");
 
     this.setState({
       name: "",
       phone: "",
       email: "",
       message: "",
+      success:[],
     });
   };
 
@@ -55,16 +57,21 @@ class Contact extends Component {
     let formErrors = this.state.formErrors;
 
     switch (name) {
-      //   case "email":
-      //     formErrors.email = emailRegex.test(value)
-      //       ? null
-      //       : "Please enter a valid email address";
-      //     break;
+        case "email":
+          formErrors.email = emailRegex.test(value)
+            ? null
+            : "Please enter a valid email address";
+          break;
 
       case "phone":
         formErrors.phone =
-          value.length < 7 ? "minimum 7 characters required" : null;
+          value.length < 7 ? "Minimum 7 characters required" : null;
         break;
+
+        case "success":
+          this.state.success[0] =
+            value.length ? "Submit Complete" : null;
+       
 
       default:
         break;
@@ -77,67 +84,85 @@ class Contact extends Component {
     return (
       <div>
         <Nav />
-        <div className="contact-container">
+        <div className="columns-container">
+            <div className="contact-left-side">
 
-        <h3 className="header1">Contact Us...</h3>
-        <h3 className="header2">We want to hear from you!</h3>
-        <br></br>
-        <div className="body">
-          Would you need music for a special event or wedding? This is the right
-          page for you! Please leave a message describing the event and the
-          genre of music you need. We will bend our strings as much as possible
-          to meet your needs!
+
+              <h3 className="header1">Contact Us...</h3>
+              <h3 className="header2">We want to hear from you!</h3>
+              <br></br>
+              <div className="body">
+                Would you need music for a special event or wedding? This is the
+                right page for you! Please leave a message describing the event
+                and the genre of music you need. We will bend our strings as
+                much as possible to meet your needs!
+              </div>
+              <div className="contact-forms">
+                <form id="contact-form" onSubmit={this.postDataHandler}>
+                  <div className="form-name">
+                    <input
+                      type="text"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                      placeholder="Fist and Last Name"
+                    />
+                  </div>
+                  <br></br>
+                  <div className="form-phone">
+                    <input
+                      type="tel"
+                      name="phone"
+                      value={this.state.phone}
+                      onChange={this.handleChange}
+                      placeholder="Cell Number: 000-000-0000 "
+                      required
+                    />
+                  
+                  </div>
+                    {formErrors.phone ? <span>{formErrors.phone}</span> : null}
+                  <br></br>
+                  <div className="form-email">
+                    <input
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      placeholder="Name@email.com"
+                      //aria-describedby="emailHelp"
+                    />
+                  </div>
+                    {formErrors.email ? <span>{formErrors.email}</span> : null}
+                  <div className="form-message">
+                    <textarea
+                      name="message"
+                      value={this.state.message}
+                      onChange={this.handleChange}
+                      placeholder="Message"
+                      rows="20"
+                    ></textarea>
+                  </div>
+                  <button type="submit">
+                    Submit
+                  </button>
+                  {console.log(this.state.phone)}
+                {!this.state.success ? <span>{this.state.success}</span> : null}
+
+
+                </form>
+                {console.log(this.state.success[0])}
+                {/* {this.state.formErrors.form ? <span>{formErrors.form[0]}</span> : null} */}
+
+              </div>
+          </div>
+
+          <div className="contact-right-side">
+     
+            <img src={shSax} className='shSax' alt="Dean St.Hillaire playing Saxophone" />
+          </div>
         </div>
-        <div className="contact-forms">
-          <form id="contact-form" onSubmit={this.postDataHandler}>
-            <div className="form-name">
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleChange}
-                placeholder="Fist and Last Name"
-              />
-            </div><br></br>
-            <div className="form-phone">
-              <input
-                type="tel"
-                name="phone"
-                value={this.state.phone}
-                onChange={this.handleChange}
-                placeholder="000-000-0000"
-                required
-              />
-              {formErrors.phone ? <span>{formErrors.phone}</span> : null}
-            </div>
-            <div className="form-email">
-              <input
-                type="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="Name@email.com"
-                aria-describedby="emailHelp"
-              />
-              {formErrors.email ? <span>{formErrors.email}</span> : null}
-            </div>
-            <div className="form-message">
-              <textarea
-                name="message"
-                value={this.state.message}
-                onChange={this.handleChange}
-                placeholder="Message"
-                rows="10"
-              ></textarea>
-            </div>
-            <button type="submit" className=".form-btn">
-              Submit
-            </button>
-          </form>
-          {formErrors.form ? <span>{formErrors.form[0]}</span> : null}
-        </div>
-        </div>
-        
+
+        <Footer />
       </div>
     );
   }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import musicPic from "../../Assets/music-stage.jpg";
 import "./StylesHome.css";
 import Nav from "../Nav/Nav";
 import ReactPlayer from "react-player";
-import Footer from '../Nav/Footer'
+import Footer from "../Nav/Footer";
 
 class Home extends Component {
   constructor(props) {
@@ -21,85 +21,82 @@ class Home extends Component {
           <Nav />
         </div>
         <div className="parent-container">
+          <div className="home-container">
+            <div className="left">
+              <div className="first-paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </div>
+              <br></br>
+              <div>
+                r adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum
+              </div>
+              <br></br>
+              <div>
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum
+              </div>
+              <br></br>
+              <div>
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </div>
+              <br></br>
+              <div>
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </div>
+            </div>
+          </div>
 
-        <div className="home-container">
-          <div className="left">
-            <div className="first-paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
+          <div className="right">
+            <div className="video1">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=vYh-Eg7AELg"
+                controls={true}
+                volume={0.1}
+                height="100%"
+                width="100%"
+                onSeek={5}
+              />
             </div>
-            <br></br>
-            <div>
-              r adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-              occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum
-            </div>
-            <br></br>
-            <div>
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-            <br></br>
-            <div>
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-            <br></br>
-            <div>
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
+
+            <div className="video2">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=8cNt8ZkRbZ4"
+                controls={true}
+                volume={0.1}
+                height="100%"
+                width="100%"
+              />
             </div>
           </div>
         </div>
-
-
-        <div className="right">
-          <div className="video1">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=vYh-Eg7AELg"
-            controls={true}
-            volume = {0.1}
-            height='100%'
-            width='100%'
-            onSeek={5}
-          />
-
-          </div>
-
-          <div className="video2">
-
-             <ReactPlayer
-            url="https://www.youtube.com/watch?v=8cNt8ZkRbZ4"
-            controls={true}
-            volume = {0.1}
-            height='100%'
-            width='100%'
-          />
-          </div>
-        </div>
-        </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
